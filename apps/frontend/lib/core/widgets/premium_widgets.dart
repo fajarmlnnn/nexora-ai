a905d530f78a04b22ac6d82d3675e6a27bf72595
fajarmlnnn <fajar.maulana1614@gmail.com>
2026-08-04 18:28:17 +0700

    fix: remove unused imports

--- apps/frontend/lib/core/widgets/premium_widgets.dart
+++ apps/frontend/lib/core/widgets/premium_widgets.dart
@@ -2,7 +2,6 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 import '../theme/app_colors.dart';
 import '../theme/app_gradients.dart';
--- apps/frontend/lib/features/dashboard/widgets/budget_summary_card.dart
+++ apps/frontend/lib/features/dashboard/widgets/budget_summary_card.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 import '../../../core/theme/app_colors.dart';
-import '../../../core/theme/app_radius.dart';
 import '../../../core/theme/app_spacing.dart';
 import '../../../core/theme/app_typography.dart';
 import '../../../core/widgets/premium_widgets.dart';
